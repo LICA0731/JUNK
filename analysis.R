a83a77e4110123213db9d21b876231ce48ab2c0e
Linda Duran <linda.hernandezduran@my.jcu.edu.au>
2021-11-29 10:18:08 +1000

    added a summary

--- analysis.R
+++ analysis.R
@@ -1,6 +1,7 @@
 x <- seq(1, 10, len = 1)
 y <- 40*2 + rnorm(10, 0, 5)
 plot(x, y)
-library(lintr)## it allows to visualize the errors in the code window
 
 
+# library(lintr)## it allows to visualize the errors in the code window
+summary(x)
